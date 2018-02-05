78d2c664978bdbc2cae5e741424cf0c28180c87a
Joshua Sohn <joshuadsohn@gmail.com>
2018-02-05 14:47:08 -0500

    adds, centers, fix, fixes background image in position

--- client/components/index.js
+++ client/components/index.js
@@ -1,6 +1,3 @@
 //'central export' for components
 export { Main } from './main';
-export { Player } from './player';
-export { Audio } from './audio';
-export { About } from './about';
 export { NavBar } from './navbar';
--- client/components/main.js
+++ client/components/main.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import { NavBar, Player, About } from './index';
 
 export const Main = (props) => {
 
   return (
     <div className="app">
-      <NavBar/>
-      <div className="main">
-        <About/>
-        <Player/>
-      </div>
-      <div className="footer">
-      <h3>Contact: <a href="mailto:mattbsohn@gmail.com?subject=RE: Voice Over Services">mattbsohn@gmail.com</a></h3>
-      </div>
     </div>
   )
 }
