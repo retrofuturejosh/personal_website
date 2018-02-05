9f6e0a2dc41ad4c63204ee996ca34f379b45b531
Joshua Sohn <joshuadsohn@gmail.com>
2018-02-05 17:48:10 -0500

    builds content and outline for main landing page

--- client/components/main.js
+++ client/components/main.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { NavBar } from './index'
 
 export const Main = (props) => {
 
   return (
     <div className="app">
+    <NavBar />
+      {/* <div id="side-bar" >
+      </div> */}
+      <div id="background-fader">
+      </div>
+      <div className="colors">
+        <div className="colors-green">
+          <div className="colors-green-title">
+            About
+          </div>
+        </div>
+        <div className="colors-purple">
+          <div className="colors-purple-title">
+              Portfolio
+          </div>
+        </div>
+        <div className="colors-blue">
+          <div className="colors-blue-title">
+            Social Media
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -4,10 +4,7 @@ export const NavBar = () => {
   return (
     <div className="navbar">
       <div className="navbar-title">
-        <h1> Josh Sohn </h1>
-      </div>
-      <div className="navbar-info">
-        <h3>Software Engineer</h3>
+        Josh Sohn
       </div>
     </div>
   )
