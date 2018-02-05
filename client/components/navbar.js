58494d8407427fe6ff2b0c6910f89cd6fa0c6108
Joshua Sohn <joshuadsohn@gmail.com>
2018-02-04 22:59:08 -0500

    adds navbar component, cleans components index, adds email link in main component

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -4,10 +4,10 @@ export const NavBar = () => {
   return (
     <div className="navbar">
       <div className="navbar-title">
-        <h1> Matt Sohn </h1>
+        <h1> Josh Sohn </h1>
       </div>
       <div className="navbar-info">
-        <h3>Voice Over Artist</h3>
+        <h3>Software Engineer</h3>
       </div>
     </div>
   )
