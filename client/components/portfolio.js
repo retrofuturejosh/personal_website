69c8ff631252089f1210a2c9c5113b1e4cf89e05
Joshua Sohn <joshuadsohn@gmail.com>
2018-02-07 12:43:20 -0500

    adds solo/team project tags to portfolio descriptions

--- client/components/portfolio.js
+++ client/components/portfolio.js
@@ -5,14 +5,17 @@ export const Portfolio = () => {
     <div className="portfolio">
       <div className="portfolio-content">
         <a href="http://www.vibezplayer.com"> Vibez Playlist Maker </a>
+          SOLO PROJECT < br/>
           Vibez is a React/Redux web-app that allows users to create Spotify and YouTube playlists based on a desired vibe, utilizing last.fm song tags.
         <br/>
         <br/>
         <a href="http://bit.ly/2FOhDMu"> Game Night </a>
+          TEAM PROJECT < br/>
           Game Night is a React/Redux web-app that allows users to play tabletop style games in a modular, interactive space with video chat.
         <br/>
         <br/>
         <a href="http://www.mattsohn.com"> MattSohn.com </a>
+          SOLO PROJECT < br/>
           I built a simple, single page website with React for professional voice over actor, Matt Sohn, based off of his design. I also recorded and engineered all of the audio examples.
       </div>
     </div>
