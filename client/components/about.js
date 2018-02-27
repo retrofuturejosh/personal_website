70715551966b761e2712da15d38aa5a327966009
Joshua Sohn <joshuadsohn@gmail.com>
2018-02-26 20:25:42 -0500

    changes about me summary

--- client/components/about.js
+++ client/components/about.js
@@ -4,7 +4,7 @@ export const About = () => {
   return (
     <div className="about">
       <div className="about-content" >
-        Greetings! I’m a full stack software engineer motivated by novel challenges, elegant design, and clean code. I recently graduated from Fullstack Academy with a fellowship through the NYC Tech Talent Pipeline. While at Fullstack, I developed advanced technical skills in a Node.js, Express, React/Redux, Sequelize tech stack. I previously worked as a music producer and audio engineer in a professional recording studio, where I consistently delivered a stellar product while nurturing positive client relationships. I am excited to continue honing and expanding my technical skills while building a career as a software engineer. 
+        Greetings! I’m a full stack software engineer and endlessly curious problem solver motivated by novel challenges, elegant design, and clean code. I attended Fullstack Academy with a full scholarship through the NYC Tech Talent Pipeline. Before starting my career as a software engineer, I worked as a music producer and audio engineer in a professional recording studio, where I consistently delivered a stellar product while nurturing positive client relationships. I am currently looking for a full time software engineer position at a company with a great product and excellent culture. 
       </div>
       <div id="resume">
         <a href="/resume.pdf"> View Resume </a>
