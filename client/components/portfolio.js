3f25c9c391462df345c4a397982dc4eabe7c6730
Joshua Sohn <joshuadsohn@gmail.com>
2018-03-09 22:49:50 -0500

    updates portfolio and resume

--- client/components/portfolio.js
+++ client/components/portfolio.js
@@ -9,6 +9,11 @@ export const Portfolio = () => {
           Vibez is a React/Redux web-app that allows users to create Spotify and YouTube playlists based on a desired vibe, utilizing last.fm song tags.
         <br/>
         <br/>
+        <a href="https://www.npmjs.com/package/react-modular-audio-player"> react-modular-audio-player </a>
+          SOLO PROJECT < br/>
+          'react-modular-audio-player' is an easily customized and styleable React Component with controls for HTML5 audio, published on NPM.
+        <br/>
+        <br/>
         <a href="http://bit.ly/2FOhDMu"> Game Night </a>
           TEAM PROJECT < br/>
           Game Night is a React/Redux web-app that allows users to play tabletop style games in a modular, interactive space with video chat.
